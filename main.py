7762b155fef569fb44a9bd4a16b5b77d53f8822a
moritayz <moritayz@keio.jp>
2025-01-06 08:12:04 +0000

    2

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
+Q=['q0', 'q1', 'q2'] #状態の有限集合
+Sigma=[0, 1] #入力記号の有限集合
+F='q2' #最終状態
+lange=4
 
-Q=['q0', 'q1', 'q2']
-Sigma=[0, 1]
-F='q2'
-
-jotai=Q[0]
-
-numbers=[]
-for i in range(4):
+numbers=[] #入力文字列を生成
+for i in range(lange+1):
     for j in range(2**i):
-        chars=list(f"{j:0{i}b}")
-        elem=list(map(lambda x:int(x), chars))
+        chars=list(f"{j:0{i}b}") #i桁の2進数(文字列)
+        elem=list(map(lambda x:int(x), chars)) #整数に変換
         numbers.append(elem)
-print(numbers)
+##print(numbers)
 
 for input in numbers:
-    for 
-        if jotai==Q[0] && input==Sigma[0]:
-            jotai=Q[0]
-        if jotai==Q[0] && input==Sigma[1]:
-            jotai=Q[1]
-        if jotai==Q[1] && input==Sigma[0]:
-            jotai=Q[2]
-        if jotai==Q[1] && input==Sigma[1]:
-            jotai=Q[0]
-        if jotai==Q[2] && input==Sigma[0]:
-            jotai=Q[0]
-        if jotai==Q[2] && input==Sigma[1]:
-            jotai=Q[1]
+    jotai=Q[0] #初期状態
+    senni=[jotai]
+    for i in input:
+        if jotai==Q[0]:
+            if i==Sigma[0]:
+                jotai=Q[0]
+            elif i==Sigma[1]:
+                jotai=Q[1]
+        elif jotai==Q[1]:
+            if i==Sigma[0]:
+                jotai=Q[2]
+            elif i==Sigma[1]:
+                jotai=Q[0]
+        elif jotai==Q[2]:
+            if i==Sigma[0]:
+                jotai=Q[0]
+            elif i==Sigma[1]:
+                jotai=Q[1]
+        senni.append(jotai)
 
-    if jotai==Q[2]:
-        print(input)'''
+    if jotai==F:
+        print("受理")
+        print(input)
+        print('->'.join(senni))
+        print()
