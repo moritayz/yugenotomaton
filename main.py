8461df672052e6a165ac055ce41c78362df4dffa
moritayz <moritayz@keio.jp>
2025-01-06 07:17:00 +0000

    2

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+
 Q=['q0', 'q1', 'q2']
 Sigma=[0, 1]
 F='q2'
 
 jotai=Q[0]
 
-numbers[0]
+numbers=[]
 for i in range(4):
     for j in range(2**i):
-        numbers.append(j)
+        chars=list(f"{j:0{i}b}")
+        elem=list(map(lambda x:int(x), chars))
+        numbers.append(elem)
 print(numbers)
 
-    '''    if jotai==Q[0] && input==Sigma[0]:
+for input in numbers:
+    for 
+        if jotai==Q[0] && input==Sigma[0]:
             jotai=Q[0]
         if jotai==Q[0] && input==Sigma[1]:
             jotai=Q[1]
