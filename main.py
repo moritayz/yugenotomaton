2292b5c1198eeb02999ca76e7ed095a0b4307307
moritayz <moritayz@keio.jp>
2025-01-06 07:00:34 +0000

    1

---
+++ main.py
@@ -0,0 +1,27 @@
+Q=['q0', 'q1', 'q2']
+Sigma=[0, 1]
+F='q2'
+
+jotai=Q[0]
+
+numbers[0]
+for i in range(4):
+    for j in range(2**i):
+        numbers.append(j)
+print(numbers)
+
+    '''    if jotai==Q[0] && input==Sigma[0]:
+            jotai=Q[0]
+        if jotai==Q[0] && input==Sigma[1]:
+            jotai=Q[1]
+        if jotai==Q[1] && input==Sigma[0]:
+            jotai=Q[2]
+        if jotai==Q[1] && input==Sigma[1]:
+            jotai=Q[0]
+        if jotai==Q[2] && input==Sigma[0]:
+            jotai=Q[0]
+        if jotai==Q[2] && input==Sigma[1]:
+            jotai=Q[1]
+
+    if jotai==Q[2]:
+        print(input)'''
